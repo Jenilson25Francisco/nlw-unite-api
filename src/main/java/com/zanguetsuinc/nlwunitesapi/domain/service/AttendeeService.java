56e0259d6b735ec5cbd8486129be043dcabcbd4f
Jenilson Francisco <franciscoviwaldo@gmail.com>
2026-04-13 15:27:42 +0100

    feat: adding pagination

--- src/main/java/com/zanguetsuinc/nlwunitesapi/domain/service/AttendeeService.java
+++ src/main/java/com/zanguetsuinc/nlwunitesapi/domain/service/AttendeeService.java
@@ -5,6 +5,7 @@ import com.zanguetsuinc.nlwunitesapi.domain.repository.AttendeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
 @Service
@@ -16,6 +17,7 @@ public class AttendeeService {
     public Attendee create(Attendee attendee){
 
         attendee.setCreatedAt(OffsetDateTime.now());
+        attendee.setChackedInAt(LocalDateTime.now());
 
         return attendeeRepository.save(attendee);
     }
